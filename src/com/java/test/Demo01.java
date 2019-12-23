de36f8b4d54cea89320cf04957791ea84d65afe9
13673998863 <13673998863@sina.cn>
2019-12-23 17:07:46 +0800

    第一次提交

---
+++ src/com/java/test/Demo01.java
@@ -0,0 +1,9 @@
+package com.java.test;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
